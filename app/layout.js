84df1b0ed98b4d0fd41b5d00889b469a4ec61101
Pushkar Gupta <pushkardps8@gmail.com>
2024-12-05 13:29:39 +0530

    setup the layout inc dark theme & footer

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,9 @@
-import localFont from "next/font/local";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import { Inter } from "next/font/google"
+import Header from "@/components/Header";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({subsets: ["latin"]})
 
 export const metadata = {
   title: "SprintFlow",
@@ -21,9 +14,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className}`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <footer className="bg-gray-900 py-12">
+            <div className="container mx-auto text-center px-4 text-gray-200">
+              <p>Made with 💗 by Pushkar Gupta</p>
+            </div>  
+          </footer>
+        </ThemeProvider>
       </body>
     </html>
   );
