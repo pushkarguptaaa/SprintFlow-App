8c94571ff49b8f7fdff33c76943e29cd06fb1095
Pushkar Gupta <pushkardps8@gmail.com>
2024-12-06 14:07:22 +0530

    Completed header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,40 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+import { SignedIn, SignedOut, SignInButton} from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "./ui/button"
+import { PenBox } from "lucide-react"
+import UserMenu from "./user-menu"
 
 const Header = () => {
   return (
-    <div>
-        <SignedOut>
-            <SignInButton />
-        </SignedOut>
+    <header className="container mx-auto">
+        <nav className="py-6 px-4 flex justify-between items-center">
+            <Link href="/">
+                <Image src={"/sf.png"} alt="SprintFlow Logo" width={200} height={56} className="h-10 w-auto object-contain"/>
+            </Link>
 
-        <SignedIn>
-            <UserButton />
-        </SignedIn>
-    </div>
+            <div className="flex items-center gap-4">
+                <Link href="/project/create">
+                    <Button variant="destructive" className="flex items-center gap-2">
+                        <PenBox size={18}/>
+                        <span>Create Project</span>
+                    </Button>
+                </Link>
+
+                <SignedOut>
+                    <SignInButton forceRedirectUrl="/onboarding">
+                        <Button variant="outline">Login</Button>
+                    </SignInButton>
+                </SignedOut>
+
+                <SignedIn>
+                    <UserMenu />
+                </SignedIn>
+            </div>
+        </nav>
+
+        
+    </header>
   )
 }
 
