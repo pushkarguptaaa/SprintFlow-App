4ac96e43c926729e941903c20d4687146ede6431
Pushkar Gupta <pushkardps8@gmail.com>
2024-12-07 16:47:08 +0530

    Completed the landing page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,113 @@
+import CompanyCarousel from "@/components/company-carousel";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { ArrowRight, BarChart, Calendar, ChevronRight, Layout } from "lucide-react";
+import Link from "next/link";
+import faqs from '@/data/faqs'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
+const features = [
+  {
+    title: "Intuitive Kanban Boards",
+    description:
+      "Visualize your workflow and optimize team productivity with our easy-to-use Kanban boards.",
+    icon: Layout,
+  },
+  {
+    title: "Powerful Sprint Planning",
+    description:
+      "Plan and manage sprints effectively, ensuring your team stays focused on delivering value.",
+    icon: Calendar,
+  },
+  {
+    title: "Comprehensive Reporting",
+    description:
+      "Gain insights into your team's performance with detailed, customizable reports and analytics.",
+    icon: BarChart,
+  },
+];
 
 export default function Home() {
   return (
-    <div>
-      hello
+    <div className="min-h-screen">
+
+      {/* Hero Section */}
+      <section className="container mx-auto py-20 text-center">
+        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold pb-6 gradient-title">
+          Streamline Your Workflow <br /> with SprintFlow
+        </h1>
+
+        <p className="text-xl text-gray-300 mb-10 max-w-3xl mx-auto">Empower your team with our intuitive project management solution.</p>
+
+        <Link href='/onboarding'>
+          <Button size='lg' className='mr-2'>Get Started <ChevronRight size={18} /></Button>
+        </Link>
+
+        <Link href='#features'>
+          <Button size='lg' variant='outline' className='mr-4'>Learn More</Button>
+        </Link>
+      </section>
+
+      {/* Features Section */}
+      <section id="features" className="bg-gray-900 py-20 px-5">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">Key Features</h3>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            {features.map((feature, index) => {
+              return (
+                <Card key={index} className="bg-gray-800">
+                  <CardContent className="pt-6">
+                    <feature.icon className="h-12 w-12 mb-4 text-blue-300"/>
+                    <h4 className="text-xl font-semibold mb-2">{feature.title}</h4>
+                    <p className="text-gray-300">{feature.description}</p>
+                  </CardContent>
+                </Card>
+              ) 
+            })}
+          </div>
+        </div>
+      </section>
+
+      {/* Testimonials Section */}
+      <section className="py-20">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">Trusted by Industry Leaders</h3>
+          <CompanyCarousel />
+        </div>
+      </section>
+
+      {/* FAQ's Section */}
+      <section className="bg-gray-900 py-20 px-5">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">Frequently Asked Questions</h3>
+            <Accordion type="single" collapsible className="w-full">
+              {faqs.map((faq, index) => {
+                return (
+                  <AccordionItem key={index} value={`item-${index}`}>
+                  <AccordionTrigger>{faq.question}</AccordionTrigger>
+                  <AccordionContent>
+                    {faq.answer}
+                  </AccordionContent>
+                </AccordionItem>
+                )
+              })}
+            </Accordion>
+        </div>
+      </section>
+
+      {/* CTA Section */}
+      <section className="py-20 text-center px-5">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-6">Ready to Transform Your Workflow?</h3>
+          <p className="text-xl mb-12">Join thousands of teams already using ZCRUM to streamline their
+          projects and boost productivity.</p>
+
+          <Link href='/onboarding'>
+              <Button size='lg' className='animate-bounce'>Start for Free <ArrowRight className="ml-2 w-5 h-5"/></Button>
+          </Link>
+        </div>
+      </section>
+
     </div>
   )
 }
