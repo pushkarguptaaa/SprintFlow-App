69a761204fa084bd63b162377cb3ba7d5c10dab1
Pushkar Gupta <pushkardps8@gmail.com>
2024-12-10 06:17:05 +0530

    Created organization page

--- app/(main)/organization/[orgId]/page.jsx
+++ app/(main)/organization/[orgId]/page.jsx
@@ -1,4 +1,5 @@
 import { getOrganization } from '@/actions/organization'
+import OrgSwitcher from '@/components/org-switcher'
 import React from 'react'
 
 const Organization = async ({params}) => {
@@ -8,10 +9,17 @@ const Organization = async ({params}) => {
     if(!organization) return <div>Organization not found</div>
   
     return (
-    <div>
-        <div>
-            <h1>{organization.name}'s Projects</h1>
+    <div className='container mx-auto'>
+        <div className='mb-4 flex flex-col sm:flex-row justify-between items-start'>
+            <h1 className='text-5xl font-bold gradient-title pb-2'>{organization.name}'s Projects</h1>
+
+            <OrgSwitcher />
         </div>
+
+        <div className='mb-4'>Show org projects</div>
+
+        <div className='mt-8'>Show user assigned and reported issues here</div>
+
     </div>
   )
 }
