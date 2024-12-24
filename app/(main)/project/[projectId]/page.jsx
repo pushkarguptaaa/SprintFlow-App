574d34bc3f5c9ffcdbc469f5efde5e74d36ffb79
Pushkar Gupta <pushkardps8@gmail.com>
2024-12-24 18:32:50 +0530

    Created get project action

--- app/(main)/project/[projectId]/page.jsx
+++ app/(main)/project/[projectId]/page.jsx
@@ -1,6 +1,16 @@
+import { getProject } from '@/actions/projects';
+import { notFound } from 'next/navigation';
 import React from 'react'
 
-const ProjectPage = () => {
+const ProjectPage = async ( {params} ) => {
+    const { projectId } = params
+
+    const project = await getProject(projectId);
+
+    if (!project) {
+      notFound();
+    }
+
   return (
     <div>ProjectPage</div>
   )
